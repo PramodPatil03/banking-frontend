ebcf28215b967e3c9f92ef1ca7d569117378fb5e
pramodpatil03 <pramodpatil190203@gmail.com>
2024-10-10 01:06:50 +0530

    Changed Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import Home from './pages/Home'
 import Login from './pages/Login'
 import CreateAccount from './pages/CreateAccount'
@@ -8,14 +8,14 @@ import Error404 from './pages/Error404'
 function App() {
   
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route exact path='/' element={<Home />} />
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/register' element={<CreateAccount />} />
         <Route exact path='*' element={<Error404 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
